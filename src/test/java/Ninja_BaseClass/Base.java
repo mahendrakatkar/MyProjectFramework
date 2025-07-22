d4375834a72fbf8756c030642697b0bf97414f65
mahendrakatkar <115349561+mahendrakatkar@users.noreply.github.com>
2025-07-22 18:27:04 +0530

    Update Base.java
    chromedrivermanagerAdded

--- src/test/java/Ninja_BaseClass/Base.java
+++ src/test/java/Ninja_BaseClass/Base.java
@@ -51,7 +51,8 @@ public class Base {
 	 */
 	public WebDriver InitializeBrowserAndOpenApplication(String browserName) {
 		if (browserName.equalsIgnoreCase("chrome")) {
-			driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mahendrakumar_Katkar\\Downloads\\chrome-win64\\chromedriver.exe");
+		driver = new ChromeDriver();
 		} else if (browserName.equalsIgnoreCase("firefox")) {
 			driver = new FirefoxDriver();
 		} else if (browserName.equalsIgnoreCase("edge")) {
